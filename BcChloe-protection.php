3201660b1be8af0a00243a61f3c9160a8d392be1
IfNoob <if.vbv.true@gmail.com>
2018-06-12 11:12:47 +0900

    Update BcChloe-protection.php

--- BcChloe-protection.php
+++ BcChloe-protection.php
@@ -2,11 +2,11 @@
 /*
 Plugin Name: BcChloe Protection
 Plugin URI: https://github.com/ifNoob/BcChloe-protection
-Description: html Not imgdrag & copy&paste & sourcecode & print protection
+Description: html not imgdrag | copy&paste | sourcecode | printscreen protection
 Author: BcChloe
 Author URI: https://bcchloe.jp
 Text Domain: bcchloe-protection
-Version: 1.0
+Version: 1.1
 License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
 
@@ -16,8 +16,8 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 /**==========================
 * Define Constants
 ===========================*/
-define( 'BC_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'BC_PLUGIN_URL', plugins_url( '' , __FILE__) );
+define( 'BC_PRO_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'BC_PRO_PLUGIN_URL', plugins_url( '' , __FILE__) );
 //global $foo;
 
 	add_action( 'init', array( 'BcChloe_Protection', 'bcchloe_protection_init' ) );
@@ -27,7 +27,7 @@ class BcChloe_Protection {
     protected $nonce_name = 'bcchloe_protection';
     protected $post_url = '';
 
-		public $plugin_version = '1.0';
+		public $plugin_version = '1.1';
 
     public static function bcchloe_protection_init() {
         new BcChloe_Protection();
@@ -42,11 +42,11 @@ class BcChloe_Protection {
 -----------------*/
 	function bc_protection() {
 		if ( !is_user_logged_in() ) {
-			wp_enqueue_style( 'bc-protection-drag', BC_PLUGIN_DIR . '/assets/css/bc-drag.css', array(), '', true );
-			wp_enqueue_script( 'bc-protection-drag', BC_PLUGIN_URL . '/assets/js/bc-img-drag.min.js' , array(), '', true );
-			wp_enqueue_script( 'bc-protection-click', BC_PLUGIN_URL . '/assets/js/bc-right-click.min.js' , array(), '', true );
-			wp_enqueue_script( 'bc-protection-print', BC_PLUGIN_URL . '/assets/js/bc-print.min.js' , array(), '', true );
-			wp_enqueue_script( 'bc-protection-prtscr', BC_PLUGIN_URL . '/assets/js/bc-prtscr.min.js' , array(), '',true );
+			wp_enqueue_style( 'bc-protection-drag', BC_PRO_PLUGIN_DIR . '/assets/css/bc-drag.css', array(), '', true );
+			wp_enqueue_script( 'bc-protection-drag', BC_PRO_PLUGIN_URL . '/assets/js/bc-img-drag.min.js' , array(), '', true );
+			wp_enqueue_script( 'bc-protection-click', BC_PRO_PLUGIN_URL . '/assets/js/bc-right-click.min.js' , array(), '', true );
+			wp_enqueue_script( 'bc-protection-print', BC_PRO_PLUGIN_URL . '/assets/js/bc-print.min.js' , array(), '', true );
+			wp_enqueue_script( 'bc-protection-prtscr', BC_PRO_PLUGIN_URL . '/assets/js/bc-prtscr.min.js' , array(), '',true );
 		}
 	}	// bc_protection
 
